5f0a44dc3419d9b8bf9ad5aee1e727a2ad64455b
Cinioluwa <covkey13@gmail.com>
2024-11-27 19:00:08 +0100

    updated port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000; // Use Render's assigned port or fallback to 5000 for local testing
 
 // Middleware
 app.use(cors());
